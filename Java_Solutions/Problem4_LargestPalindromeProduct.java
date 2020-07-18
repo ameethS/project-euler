ab8e72a364ab3391dfb4d4360fe890db905b7e95
ameethS <49598722+ameethS@users.noreply.github.com>
2020-07-18 16:24:02 +0200

    Update Problem4_LargestPalindromeProduct.java
    comments added

--- Java_Solutions/Problem4_LargestPalindromeProduct.java
+++ Java_Solutions/Problem4_LargestPalindromeProduct.java
@@ -7,18 +7,20 @@ public class Problem4_LargestPalindromeProduct {
     */
     public static int reverse(int n){
         String initial = n + ""; //convert the int to string
-        String reversed = "";
-        for (int i = initial.length() - 1; i > -1; i--){
+        String reversed = ""; //var reversed initialized to empty string
+        for (int i = initial.length() - 1; i > -1; i--){ //loop through all characters in string initial (last to first)
             reversed = reversed + initial.charAt(i);
         }
-        return Integer.parseInt(reversed);
+        return Integer.parseInt(reversed); //return reversed string converted to integer
     }
 
+    /*
+    takes in int n, returns true if n is equal to n reversed
+    */
     public static boolean isPalindromic(int n){
-        boolean pal = false;
         if (n == reverse(n)) //if n is the same as n reversed
-            pal = true;
-        return pal;
+            return true;
+        return false;
     }
 
     public static void main(String[] args){
@@ -26,12 +28,12 @@ public class Problem4_LargestPalindromeProduct {
        // int b = 99;
         int product = 1;
 	//check all combinations
-        for (int a = 100; a < 1000; a++){
-            for (int b = 100; b < 1000; b++){
-                product = a * b;
+        for (int a = 100; a < 1000; a++){ //check all 3 digit ints (100 - 999)
+            for (int b = 100; b < 1000; b++){ //check all 3 digit ints (100 - 999)
+                product = a * b; //product of two 3-digit numbers
                 //System.out.println(product);
-                if (isPalindromic(product) == true){
-                    if (product > largest)
+                if (isPalindromic(product) == true) { //if the product is the same as its reverse
+                    if (product > largest) //if this is the largest we've encountered thus far, then let var largest equal to it
                         largest = product;
                 }
             }
